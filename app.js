11ad43bd44a775e734bb35fdcc581b6d1589d5df
Charlie <charlieedoherty@gmail.com>
2024-07-14 22:18:20 -0500

    Create a form that posts data to the server

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const app = express()
 
+app.use(express.urlencoded({extended: false}))
+
 app.get("/", function (req, res) {
-    res.send("<h1>Hello</h1>")
+    res.send("<form action='/store-user' method='POST'><input type='text' name='username'><button>Submit</button></form>")
 })
 
 app.get("/today", function (req, res) {
     res.send("<h1>" + new Date().toISOString() + "</h1>")
+})
+
+app.post("/store-user", function (req, res) {
+    const userName = req.body.username
+    res.send("<h1>" + userName + "</h1>")
 })
 
 app.listen(3000)
